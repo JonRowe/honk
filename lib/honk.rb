5747a8da897b6f52efef7563c67d6d14a00f35e9
Andrew Radev <andrey.radev@gmail.com>
2019-12-12 15:27:50 +0200

    Run players with no output

--- lib/honk.rb
+++ lib/honk.rb
@@ -4,12 +4,16 @@ module Honk
   module_function
 
   def call
-    if system('which afplay', out: '/dev/null', err: '/dev/null')
-      `afplay #{HONK}`
-    elsif system('which mplayer', out: '/dev/null', err: '/dev/null')
-      `mplayer #{HONK}`
+    if run_quietly('which afplay')
+      run_quietly("afplay #{HONK}")
+    elsif run_quietly('which mplayer')
+      run_quietly("mplayer #{HONK}")
     else
       raise "No `afplay` or `mplayer` executable"
     end
+  end
+
+  def run_quietly(command)
+    system(command, out: '/dev/null', err: '/dev/null')
   end
 end
